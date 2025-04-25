2a34e0b86cde0a8a721d0208da4aa3fb7b66b1ba
AndreFontaine <andres.gonzalez.ud@gmail.com>
2025-04-25 13:29:57 +0200

    Add bath data

--- config/data.js
+++ config/data.js
@@ -440,6 +440,536 @@ export const initialFoodData = [
     "note": "",
     "milkType": "breast",
     "volume": "70"
+  },
+  {
+    "id": 1745256888276,
+    "type": "breast",
+    "date": "2025-04-21",
+    "time": "19:32",
+    "note": "Sans silicone ",
+    "duration": {
+      "left": 0,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745262027318,
+    "type": "bottle",
+    "date": "2025-04-21",
+    "time": "21:00",
+    "note": "",
+    "milkType": "formula",
+    "volume": "100"
+  },
+  {
+    "id": 1745262060087,
+    "type": "breast",
+    "date": "2025-04-21",
+    "time": "20:00",
+    "note": "",
+    "duration": {
+      "left": 10,
+      "right": 10
+    }
+  },
+  {
+    "id": 1745266839330,
+    "type": "breast",
+    "date": "2025-04-21",
+    "time": "21:15",
+    "note": "",
+    "duration": {
+      "left": 5,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745280552326,
+    "type": "breast",
+    "date": "2025-04-22",
+    "time": "02:00",
+    "note": "Avec silicone ",
+    "duration": {
+      "left": 7,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745290670379,
+    "type": "breast",
+    "date": "2025-04-22",
+    "time": "04:54",
+    "note": "Avec silicone /sans silicone ",
+    "duration": {
+      "left": 0,
+      "right": 13
+    }
+  },
+  {
+    "id": 1745292772682,
+    "type": "bottle",
+    "date": "2025-04-22",
+    "time": "05:25",
+    "note": "",
+    "milkType": "formula",
+    "volume": "60"
+  },
+  {
+    "id": 1745302943397,
+    "type": "breast",
+    "date": "2025-04-22",
+    "time": "08:15",
+    "note": "",
+    "duration": {
+      "left": 10,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745314165410,
+    "type": "breast",
+    "date": "2025-04-22",
+    "time": "10:30",
+    "note": "",
+    "duration": {
+      "left": 5,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745314205816,
+    "type": "bottle",
+    "date": "2025-04-22",
+    "time": "11:27",
+    "note": "",
+    "milkType": "formula",
+    "volume": "105"
+  },
+  {
+    "id": 1745335986936,
+    "type": "breast",
+    "date": "2025-04-22",
+    "time": "15:25",
+    "note": "",
+    "duration": {
+      "left": 10,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745342029571,
+    "type": "bottle",
+    "date": "2025-04-22",
+    "time": "19:00",
+    "note": "",
+    "milkType": "formula",
+    "volume": "60"
+  },
+  {
+    "id": 1745343191368,
+    "type": "bottle",
+    "date": "2025-04-22",
+    "time": "19:33",
+    "note": "",
+    "milkType": "formula",
+    "volume": "40"
+  },
+  {
+    "id": 1745353347308,
+    "type": "breast",
+    "date": "2025-04-22",
+    "time": "22:00",
+    "note": "",
+    "duration": {
+      "left": 10,
+      "right": 10
+    }
+  },
+  {
+    "id": 1745355132549,
+    "type": "bottle",
+    "date": "2025-04-22",
+    "time": "22:30",
+    "note": "",
+    "milkType": "formula",
+    "volume": "75"
+  },
+  {
+    "id": 1745358892763,
+    "type": "breast",
+    "date": "2025-04-22",
+    "time": "23:54",
+    "note": "Avec silicone ",
+    "duration": {
+      "left": 5,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745372381075,
+    "type": "breast",
+    "date": "2025-04-23",
+    "time": "03:30",
+    "note": "Avec silicone ",
+    "duration": {
+      "left": 0,
+      "right": 8
+    }
+  },
+  {
+    "id": 1745383243999,
+    "type": "breast",
+    "date": "2025-04-23",
+    "time": "05:30",
+    "note": "Avec silicone sans silicone ",
+    "duration": {
+      "left": 8,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745383298155,
+    "type": "bottle",
+    "date": "2025-04-23",
+    "time": "06:35",
+    "note": "",
+    "milkType": "formula",
+    "volume": "120"
+  },
+  {
+    "id": 1745394048340,
+    "type": "breast",
+    "date": "2025-04-23",
+    "time": "09:35",
+    "note": "Sans silicone ",
+    "duration": {
+      "left": 0,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745398145673,
+    "type": "bottle",
+    "date": "2025-04-23",
+    "time": "10:30",
+    "note": "",
+    "milkType": "breast",
+    "volume": "80"
+  },
+  {
+    "id": 1745412301404,
+    "type": "bottle",
+    "date": "2025-04-23",
+    "time": "12:00",
+    "note": "",
+    "milkType": "formula",
+    "volume": "70"
+  },
+  {
+    "id": 1745420428187,
+    "type": "breast",
+    "date": "2025-04-23",
+    "time": "17:00",
+    "note": "Sans silicone ",
+    "duration": {
+      "left": 5,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745422138898,
+    "type": "breast",
+    "date": "2025-04-23",
+    "time": "17:28",
+    "note": "Sans silicone ",
+    "duration": {
+      "left": 5,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745427004918,
+    "type": "bottle",
+    "date": "2025-04-23",
+    "time": "18:35",
+    "note": "",
+    "milkType": "breast",
+    "volume": "40"
+  },
+  {
+    "id": 1745429685596,
+    "type": "bottle",
+    "date": "2025-04-23",
+    "time": "19:30",
+    "note": "",
+    "milkType": "breast",
+    "volume": "50"
+  },
+  {
+    "id": 1745447453126,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "00:22",
+    "note": "Sans silicona ",
+    "duration": {
+      "left": 5,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745458823791,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "00:00",
+    "note": "",
+    "duration": {
+      "left": 5,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745458859423,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "02:15",
+    "note": "Sans silicona ",
+    "duration": {
+      "left": 0,
+      "right": 2
+    }
+  },
+  {
+    "id": 1745458896736,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "03:38",
+    "note": "Dans silicone ",
+    "duration": {
+      "left": 6,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745463512646,
+    "type": "bottle",
+    "date": "2025-04-24",
+    "time": "04:55",
+    "note": "",
+    "milkType": "formula",
+    "volume": "70"
+  },
+  {
+    "id": 1745474888310,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "07:40",
+    "note": "",
+    "duration": {
+      "left": 0,
+      "right": 3
+    }
+  },
+  {
+    "id": 1745480633918,
+    "type": "bottle",
+    "date": "2025-04-24",
+    "time": "09:30",
+    "note": "",
+    "milkType": "formula",
+    "volume": "100"
+  },
+  {
+    "id": 1745484491242,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "10:45",
+    "note": "",
+    "duration": {
+      "left": 8,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745493114712,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "12:00",
+    "note": "Avec silicona nueva ",
+    "duration": {
+      "left": 0,
+      "right": 8
+    }
+  },
+  {
+    "id": 1745496985518,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "14:15",
+    "note": "",
+    "duration": {
+      "left": 5,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745498976188,
+    "type": "bottle",
+    "date": "2025-04-24",
+    "time": "14:45",
+    "note": "+Calmosine ",
+    "milkType": "breast",
+    "volume": "55"
+  },
+  {
+    "id": 1745499294663,
+    "type": "bottle",
+    "date": "2025-04-24",
+    "time": "14:54",
+    "note": "",
+    "milkType": "formula",
+    "volume": "45"
+  },
+  {
+    "id": 1745501167258,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "15:25",
+    "note": "",
+    "duration": {
+      "left": 5,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745506384766,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "16:38",
+    "note": "Avec silicone ",
+    "duration": {
+      "left": 10,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745508170132,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "17:00",
+    "note": "",
+    "duration": {
+      "left": 20,
+      "right": 5
+    }
+  },
+  {
+    "id": 1745511189850,
+    "type": "bottle",
+    "date": "2025-04-24",
+    "time": "18:13",
+    "note": "",
+    "milkType": "formula",
+    "volume": "70"
+  },
+  {
+    "id": 1745519790017,
+    "type": "bottle",
+    "date": "2025-04-24",
+    "time": "20:25",
+    "note": "",
+    "milkType": "breast",
+    "volume": "10"
+  },
+  {
+    "id": 1745528990671,
+    "type": "breast",
+    "date": "2025-04-24",
+    "time": "22:55",
+    "note": "Al comenzar silicona ",
+    "duration": {
+      "left": 10,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745541726684,
+    "type": "breast",
+    "date": "2025-04-25",
+    "time": "02:40",
+    "note": "Avec silicone ",
+    "duration": {
+      "left": 3,
+      "right": 7
+    }
+  },
+  {
+    "id": 1745543822396,
+    "type": "bottle",
+    "date": "2025-04-25",
+    "time": "03:09",
+    "note": "",
+    "milkType": "formula",
+    "volume": "75"
+  },
+  {
+    "id": 1745553850202,
+    "type": "breast",
+    "date": "2025-04-25",
+    "time": "06:03",
+    "note": "",
+    "duration": {
+      "left": 0,
+      "right": 10
+    }
+  },
+  {
+    "id": 1745554626681,
+    "type": "bottle",
+    "date": "2025-04-25",
+    "time": "06:16",
+    "note": "",
+    "milkType": "breast",
+    "volume": "30"
+  },
+  {
+    "id": 1745562007345,
+    "type": "breast",
+    "date": "2025-04-25",
+    "time": "08:15",
+    "note": "",
+    "duration": {
+      "left": 5,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745574099309,
+    "type": "bottle",
+    "date": "2025-04-25",
+    "time": "12:12",
+    "note": "",
+    "milkType": "formula",
+    "volume": "65"
+  },
+  {
+    "id": 1745575050653,
+    "type": "breast",
+    "date": "2025-04-25",
+    "time": "11:55",
+    "note": "Sans silicone ",
+    "duration": {
+      "left": 3,
+      "right": 0
+    }
+  },
+  {
+    "id": 1745575304535,
+    "type": "bottle",
+    "date": "2025-04-25",
+    "time": "09:30",
+    "note": "",
+    "milkType": "formula",
+    "volume": "85"
   }
 ];
 
@@ -674,6 +1204,139 @@ export const initialDiaperData = [
     "time": "13:00",
     "note": "Urine aussi ",
     "type": "poop"
+  },
+  {
+    "id": 1745256820921,
+    "date": "2025-04-21",
+    "time": "19:30",
+    "note": "Urine aussi ",
+    "type": "poop"
+  },
+  {
+    "id": 1745266780456,
+    "date": "2025-04-21",
+    "time": "21:30",
+    "note": "Urine aussi ",
+    "type": "poop"
+  },
+  {
+    "id": 1745292752848,
+    "date": "2025-04-22",
+    "time": "05:10",
+    "note": "Traza de popó ",
+    "type": "pee"
+  },
+  {
+    "id": 1745314186080,
+    "date": "2025-04-22",
+    "time": "11:15",
+    "note": "Traza de popó ",
+    "type": "pee"
+  },
+  {
+    "id": 1745335914969,
+    "date": "2025-04-22",
+    "time": "13:40",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745335943909,
+    "date": "2025-04-22",
+    "time": "15:20",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745353395515,
+    "date": "2025-04-22",
+    "time": "21:55",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745353466702,
+    "date": "2025-04-22",
+    "time": "20:15",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745383269437,
+    "date": "2025-04-23",
+    "time": "06:00",
+    "note": "Traza de popó ",
+    "type": "pee"
+  },
+  {
+    "id": 1745398107811,
+    "date": "2025-04-23",
+    "time": "10:45",
+    "note": "Urine aussi ",
+    "type": "poop"
+  },
+  {
+    "id": 1745422121861,
+    "date": "2025-04-23",
+    "time": "17:25",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745429709049,
+    "date": "2025-04-23",
+    "time": "19:30",
+    "note": "Urine aussi ",
+    "type": "poop"
+  },
+  {
+    "id": 1745463447536,
+    "date": "2025-04-24",
+    "time": "04:30",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745484073295,
+    "date": "2025-04-24",
+    "time": "10:35",
+    "note": "Bcp du selles, urine aussi ",
+    "type": "poop"
+  },
+  {
+    "id": 1745499312450,
+    "date": "2025-04-24",
+    "time": "14:35",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745511178263,
+    "date": "2025-04-24",
+    "time": "18:05",
+    "note": "",
+    "type": "poop"
+  },
+  {
+    "id": 1745529636235,
+    "date": "2025-04-24",
+    "time": "23:15",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745553832187,
+    "date": "2025-04-25",
+    "time": "06:03",
+    "note": "",
+    "type": "pee"
+  },
+  {
+    "id": 1745575021949,
+    "date": "2025-04-25",
+    "time": "11:50",
+    "note": "Mini traza de popó ",
+    "type": "pee"
   }
 ];
 
@@ -773,32 +1436,47 @@ export const initialPumpData = [
     "time": "15:50",
     "volume": "55",
     "type": "pump"
+  },
+  {
+    "id": 1745295236509,
+    "duration": "30",
+    "date": "2025-04-22",
+    "time": "05:40",
+    "volume": "50",
+    "type": "pump"
+  },
+  {
+    "id": 1745386037908,
+    "duration": "23",
+    "date": "2025-04-23",
+    "time": "06:50",
+    "volume": "73",
+    "type": "pump"
+  },
+  {
+    "id": 1745506546225,
+    "duration": "27",
+    "date": "2025-04-24",
+    "time": "13:30",
+    "volume": "85",
+    "type": "pump"
+  },
+  {
+    "id": 1745578359634,
+    "duration": "29",
+    "date": "2025-04-25",
+    "time": "12:25",
+    "volume": "75",
+    "type": "pump"
   }
 ];
 
 export const initialBathData = [
   {
-    "id": 1745043758300,
-    "date": "2025-04-15",
-    "time": "16:30",
-    "type": "bath"
-  },
-  {
-    "id": 1745043758301,
-    "date": "2025-04-08",
-    "time": "16:00",
-    "type": "bath"
-  },
-  {
-    "id": 1745043758302,
-    "date": "2025-04-01",
+    "id": 1745577470723,
+    "date": "2025-04-23",
     "time": "15:30",
-    "type": "bath"
-  },
-  {
-    "id": 1745043758303,
-    "date": "2025-03-15",
-    "time": "15:20",
-    "type": "bath"
+    "type": "bath",
+    "note": "Miércoles "
   }
 ];
